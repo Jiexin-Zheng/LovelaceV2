4ace0177cb813fbdceb58f0bf41a50984fae8a00
Jsutinen <Joonas.sutinen@hotmail.com>
2021-03-22 16:34:07 +0200

    Separated the different content types from the main model into subclasses

--- Lovelacev2/LovelaceContentPage/models.py
+++ Lovelacev2/LovelaceContentPage/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class LectureContent(models.Model):   # A Database model for the content on the page (Should probably be turned into a parent class with only Parent, index and content type)
     Parent = models.CharField(max_length=100,) # Parent field pointing to the lecture/course that the content is part of
     Index = models.PositiveIntegerField() # index of the content on the webpage. When the website is printed it goes through for loop and prints the content from index 0 -> 
     ContentType = models.CharField(max_length=100,) #Content type, Would be like image/text/ etc embedded content
-    ContentText = models.CharField(max_length=1000,) #Content text, Should be only for the text content, probably should be in separate subclass that has this one as parent
-    ContentHeader = models.CharField(max_length=100,) #Also only for text content. The header of the text content (Optional)
-    ContentImageTitle = models.CharField(max_length=100,) #For Image content the title of the image (Should be subclass for image)
-    ContentImageFile = models.ImageField(upload_to="static") #The file to be shown on the website
+
+class TextContentModel(LectureContent):
+    ContentText = models.CharField(max_length=1000,)
+    ContentHeader = models.CharField(max_length=100,)
+	
+class ImageContentModel(LectureContent):
+    ContentImageTitle = models.CharField(max_length=100,)
+    ContentImageFile = models.ImageField(upload_to="static")
     def __str__(self): #A function that prints the queryset in shell
-        string = str(self.Index) + self.ContentHeader + self.ContentText
-        return (string)
-    
+        string = self.ContentImageTitle + self.ContentImageFile.url
+        return(string)
--- Lovelacev2/LovelaceContentPage/views.py
+++ Lovelacev2/LovelaceContentPage/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Template, loader, engines
 from .forms import TextForm, DeleteForm, ImageFileForm
-from .models import LectureContent
+from .models import LectureContent, ImageContentModel, TextContentModel
 
 def index(request):
     List = []       #This list will contain all of the content to be rendered on the selected lecture
     List.clear() 
     indexnumber = 0   # index that will hold the max number of separate pieces of content on the website
     isBreak = False # No idea why this is here
-    LectureContentObjects = LectureContent.objects.filter(Parent = "Lecture8")   # LectureContentObjects filters the lecture content from database that belongs to the selected course. In this case the "lecture8" is just an example, in real implementation it would be somehow inherited from the website that you can get to the lecture from.
+    LectureContentObjects = LectureContent.objects.filter(Parent = "Lecture8")  # LectureContentObjects filters the lecture content from database that belongs to the selected course. In this case the "lecture8" is just an example, in real implementation it would be somehow inherited from the website that you can get to the lecture from.
     SortedLectureContentObjects = LectureContentObjects.order_by("Index")  #Sort the lecture specific content based on index so that it gets rendered in correct order.
     for i in SortedLectureContentObjects:    #a for loop that saves all the sorted lecture content into the list in order plus changes the indexes if there are "holes" in the order of them. for example if content with index 2 is deleted then the following indexes must be changed to make it work.
         if i.Index != indexnumber:   #this is the reordering part.
@@ -30,7 +30,7 @@ def index(request):
                 if j.Index >= enteredindex:
                     j.Index += 1
                     j.save()
-            content = LectureContent(Parent = "Lecture8", Index = enteredindex, ContentType = "Text", ContentText = text, ContentHeader = header, ContentImageFile = "NoFile", ContentImageTitle = "NoTitle")
+            content = TextContentModel(Parent = "Lecture8", Index = enteredindex, ContentType = "Text", ContentText = text, ContentHeader = header)
             content.save() #content is saved into database
             context = { 
                 "List": List,
@@ -56,7 +56,7 @@ def index(request):
                 if j.Index >= enteredindex:
                     j.Index += 1
                     j.save()
-            content = LectureContent(Parent = "Lecture8", Index = enteredindex, ContentType = "Image", ContentText = "NoText", ContentHeader = "NoHeader", ContentImageFile = imagefile, ContentImageTitle = imagefiletitle)
+            content = ImageContentModel(Parent = "Lecture8", Index = enteredindex, ContentType = "Image", ContentImageTitle = imagefiletitle, ContentImageFile = imagefile)
             content.save()
             print(content)
             return HttpResponseRedirect("/LovelaceContentPage/")  #redirect back to the same page(reload page)
